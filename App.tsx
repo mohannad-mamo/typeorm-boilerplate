63e5d07f046e43d8edf9a3daa304dc83f5aff70f
Mohannad Mamo <mohannad@Mohannads-MacBook-Pro.local>
2019-03-15 13:06:10 +0100

    sqlite working with both IOS & Android

---
+++ App.tsx
@@ -0,0 +1,65 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * 
+ * Generated with the TypeScript template
+ * https://github.com/emin93/react-native-template-typescript
+ * 
+ * @format
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import SQLite from 'react-native-sqlite-storage';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+interface Props {}
+export default class App extends Component<Props> {
+  errorCB(err: any) {
+    console.warn("SQL Error: " + err);
+  }
+  
+  successCB() {
+    console.warn("SQL executed fine");
+  }
+
+  componentDidMount(){
+    SQLite.openDatabase({name: 'my.db', location: 'Library'}, this.successCB, this.errorCB);
+  }
+
+  
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>To get started, edit App.tsx</Text>
+        <Text style={styles.instructions}>{instructions}</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
